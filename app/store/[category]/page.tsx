d5d1a7df616c672e26ba4c36cb4534df279e186f
Adil Ashurov <ashurovadil96@gmail.com>
2024-11-14 23:32:21 +0400

    +

--- app/store/[category]/page.tsx
+++ app/store/[category]/page.tsx
@@ -2,16 +2,10 @@ import React from "react";
 import { getProducts } from "@/app/services/getProducts";
 import ProductList from "@/components/ProductList";
 
-interface PageProps {
-  params: {
-    category: string;
-  };
-}
+export default async function Page({ params }: { params: any }) {
+  const category = await params;
 
-export default async function Page({ params }: PageProps) {
-  const slug = params.category;
-
-  const data = await getProducts(slug);
+  const data = await getProducts(category);
 
   return (
     <section className="relative w-[96%] flex items-center justify-center mt-8 flex-col">
