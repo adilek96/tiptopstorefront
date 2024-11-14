431412de70d5be68d554a6e9fb1ebbca7dbb8e91
Adil Ashurov <ashurovadil96@gmail.com>
2024-11-14 23:59:32 +0400

    +

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ['http://194.163.151.112:9002/'], // Замените на свои домены
+  },
 };
 
 export default nextConfig;
