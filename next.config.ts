a068c7f15034bfedbf4b9e33aebaa24a30820e5a
Adil Ashurov <ashurovadil96@gmail.com>
2024-11-15 00:40:14 +0400

    delivery img

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
-    remotePatterns: [{hostname:'http://194.163.151.112:9002/'}], // Замените на свои домены
+    remotePatterns: [
+      {
+        protocol: 'http', // Можно указать 'https' или оставить пустым для обоих
+        hostname: '194.163.151.112',
+        port: '9002', // Укажите порт, если он используется
+        pathname: '/**', // Разрешает любые пути на этом домене и порту
+      },
+    ],
   },
 };
-
 export default nextConfig;
