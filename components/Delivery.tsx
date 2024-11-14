96b07e6362e97887b46aa7025631b99bbe39f7b4
Adil Ashurov <ashurovadil96@gmail.com>
2024-11-15 00:38:51 +0400

    delivery img

--- components/Delivery.tsx
+++ components/Delivery.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import DeliveryTruck from "@/public/DeliveryTruck";
+import Image from "next/image";
 
 export default function Delivery() {
   return (
@@ -7,31 +7,50 @@ export default function Delivery() {
       <div className="ribbon-del flex justify-center absolute top-10 left-0 z-50">
         <span className="py-1">Доставка</span>
       </div>
-      <div className="w-full py-20 bg-gray-200 flex flex-wrap text-[12px] text-center font-semibold justify-around items-center">
-        <div className="w-64 h-48 flex justify-center flex-col ">
-          <div className="w-full h-32 bg-red-500 "></div>
-          <div className="w-full h-16">
-            <p className="cursor-default">
+      <div className="w-full pt-20 pb-10 shadow-2xl flex flex-wrap text-[12px] text-center font-semibold justify-around items-center">
+        <div className="w-[300px]  flex justify-center flex-col ">
+          <div className="w-full flex justify-center  mb-3 ">
+            <Image
+              src={"/deliveryman1.jpg"}
+              alt="Futuristic delivery"
+              width={200}
+              height={200}
+            />
+          </div>
+          <div className="w-full ">
+            <p className="cursor-default text-lg">
               Бесплатная доставка до любой станции метро, в любое удобное для
               вас время.
             </p>
           </div>
         </div>
-        <div className="w-64 h-48 flex justify-center flex-col ">
-          <div className="w-full h-32 ">
-            <DeliveryTruck />
+        <div className="w-[300px]  flex justify-center flex-col ">
+          <div className="w-full flex justify-center mb-3  ">
+            <Image
+              src={"/deliveryman3.jpg"}
+              alt="Futuristic delivery"
+              width={200}
+              height={200}
+            />
           </div>
-          <div className="w-full h-16 ">
-            <p className="cursor-default">
+          <div className="w-full ">
+            <p className="cursor-default text-lg">
               Доставка до адреса в черте города Баку в любое удобное для вас
               время.
             </p>
           </div>
         </div>
-        <div className="w-64 h-48 flex justify-center flex-col">
-          <div className="w-full h-32 bg-red-500 "></div>
-          <div className="w-full h-16 ">
-            <p className="cursor-default">
+        <div className="w-[300px]  flex justify-center flex-col">
+          <div className="w-full flex justify-center mb-3  ">
+            <Image
+              src={"/deliveryman2.jpg"}
+              alt="Futuristic delivery"
+              width={200}
+              height={200}
+            />
+          </div>
+          <div className="w-full  ">
+            <p className="cursor-default text-lg">
               Отправка заказов в районы почтой или курьерскими службами.
             </p>
           </div>
